e659e233e207add965698cc223a00c4ccd694a63
RupeshKalkani <77331571+RupeshKalkani@users.noreply.github.com>
2022-07-02 20:18:38 +0530

    upload project

---
+++ android/app/src/main/kotlin/com/example/amira_ecommerce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amira_ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:amira_ecommerce/screen/home/home_page.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() => runApp(GetMaterialApp(
+      home: HomePage(),
+    ));
